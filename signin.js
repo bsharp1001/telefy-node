b4a85457d7bee2ef216872a50132698ebe1c1f1a
root <root@localhost.localdomain>
2020-06-20 22:49:50 +0200

    hndled duplicate users error

--- signin.js
+++ signin.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 var LocalStorage = require('node-localstorage').LocalStorage,
   localStorage = new LocalStorage('./data');
 
-  /*
+
 var connection;
 function handleDisconnect() {
   connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL); // Recreate the connection, since
@@ -28,13 +28,6 @@ function handleDisconnect() {
 }
 
 handleDisconnect();
-*/
-var connection = mysql.createConnection({
-  host     : "localhost",
-  user : "root",
-  database : "telegram",
-});
-connection.connect();
 
 var values = [];
 
