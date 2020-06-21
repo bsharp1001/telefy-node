0ce96543e5cc6f2df7b502b0a626334b4d01dd97
root <root@localhost.localdomain>
2020-06-21 14:22:56 +0200

    hndled duplicate users error

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ var LocalStorage = require('node-localstorage').LocalStorage,
 
 var connection;
 function handleDisconnect() {
-  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL); // Recreate the connection, since
+  connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL); // Recreate the connection, since
   // the old one cannot be reused.
 
-  connection.connect(function (err) {              // The server is either down
-    if (err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
+  //connection.connect(function (err) {              // The server is either down
+  //  if (err) {                                     // or restarting (takes a while sometimes).
+  //    console.log('error when connecting to db:', err);
+  //    setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+  //  }                                     // to avoid a hot loop, and to allow our node script to
+  //});                                     // process asynchronous requests in the meantime.
   // If you're also serving http, display a 503 error.
   connection.on('error', function (err) {
     console.log('db error', err);
